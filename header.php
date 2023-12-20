b04e4ef2a634cd0fe920f645c21334ed282e7b4f
Sujit Bhuva <sujit@xtendify.com>
2023-12-20 12:55:50 +0530

    demo-components

--- header.php
+++ header.php
@@ -1,66 +1,108 @@
 <?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Simple_Calendar_Theme
- */
-?>
+   /**
+    * The header for our theme
+    *
+    * This is the template that displays all of the <head> section and everything up until <div id="content">
+    *
+    * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+    *
+    * @package Simple_Calendar_Theme
+    */
+   
+   ?>
 <!doctype html>
-
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'simple-calendar' ); ?></a>
-
-	<header id="masthead" class="site-header main_header">
-		<div class="header_wrapper">
-		
-		<div class="site-branding sc_logo">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$simple_calendar_description = get_bloginfo( 'description', 'display' );
-			if ( $simple_calendar_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $simple_calendar_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-		<div class="toggle">
-					<div class="menu-bar1 bar"></div>
-					<div class="menu-bar2 bar"></div>
-					<div class="menu-bar3 bar"></div>
-          </div>
-		</div>
-	</header><!-- #masthead -->
+   <head>
+      <meta charset="<?php bloginfo( 'charset' ); ?>">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="profile" href="https://gmpg.org/xfn/11">
+      <?php wp_head(); ?>
+   </head>
+   <body <?php body_class(); ?>>
+      <?php wp_body_open(); ?>
+      <div id="page" class="site">
+      <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'simple-calendar' ); ?></a>
+      <?php
+         $header = 'demo';
+         if($header == 'production'){ ?>
+      <header id="masthead" class="sc_animate_header site-header main_header">
+         <div class="header_wrapper">
+            <div class="site-branding sc_logo">
+               <?php
+                  the_custom_logo();
+                  if ( is_front_page() && is_home() ) :
+                  	?>
+               <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+               <?php
+                  else :
+                  	?>
+               <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+               <?php
+                  endif;
+                  $simple_calendar_description = get_bloginfo( 'description', 'display' );
+                  if ( $simple_calendar_description || is_customize_preview() ) :
+                  	?>
+               <p class="site-description"><?php echo $simple_calendar_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+               <?php endif; ?>
+            </div>
+            <!-- .site-branding -->
+            <nav id="site-navigation" class="main-navigation">
+               <?php
+                  wp_nav_menu(
+                  	array(
+                  		'theme_location' => 'menu-1',
+                  		'menu_id'        => 'primary-menu',
+                  	)
+                  );
+                  ?>
+            </nav>
+            <!-- #site-navigation -->
+            <div class="toggle">
+               <div class="menu-bar1 bar"></div>
+               <div class="menu-bar2 bar"></div>
+               <div class="menu-bar3 bar"></div>
+            </div>
+         </div>
+      </header>
+      <!-- #masthead -->
+      <?php } else {?>
+      <header id="masthead" class="demo_sc_main_header">
+         <div class="header_wrapper">
+            <div class="site-branding demo_sc_logo">
+               <?php
+                  the_custom_logo();
+                  if ( is_front_page() && is_home() ) :
+                  	?>
+               <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+               <?php
+                  else :
+                  	?>
+               <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+               <?php
+                  endif;
+                  $simple_calendar_description = get_bloginfo( 'description', 'display' );
+                  if ( $simple_calendar_description || is_customize_preview() ) :
+                  	?>
+               <p class="site-description"><?php echo $simple_calendar_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+               <?php endif; ?>
+            </div>
+            <!-- .site-branding -->
+            <nav id="site-navigation" class="main-navigation demo_sc_nav">
+               <?php
+                  wp_nav_menu(
+                  	array(
+                  		'theme_location' => 'menu-1',
+                  		'menu_id'        => 'primary-menu',
+                  	)
+                  );
+                  ?>
+            </nav>
+            <!-- #site-navigation -->
+            <div class="demo_sc_toggle">
+               <div class="menu-bar1 bar"></div>
+               <div class="menu-bar2 bar"></div>
+               <div class="menu-bar3 bar"></div>
+            </div>
+         </div>
+      </header>
+      <?php }
---
+++ page-templates/demo_sc.php
@@ -0,0 +1,147 @@
+<?php
+   /*
+    * Template Name:  Demo-sc
+    */
+    
+   get_header(); ?>
+<?php $file_path = get_template_directory_uri(); ?> 
+<div class="container">
+   <div class="demo_sc_wrapper">
+      <div class="row">
+         <div class="demo_sc_center_title">
+            <h2 class="demo_sc_h2">Here’s a basic calendar using the grid view.</h2>
+            <p class="demo_sc_main_para">Here the event bubbles (tooltips) are set to display upon mouse hover, but they can also be set to display upon mouse click.</p>
+            <p class="demo_sc_main_para">On <span class="demo_sc_span_p">mobile</span> <span class="demo_sc_span_b">devices</span> devices a tap is <span class="demo_sc_span_g">always</span> required.</p>
+            <a href="" class="demo_sc_primary_btn">Download the add-on here.</a>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-lg-7">
+            <div class="row">
+               <div class="col-lg-6">
+                  <div class="demo_sc_lv_ul">
+                     <h3 class="demo_sc_h3">Grid View </h3>
+                     <ul>
+                        <li><a href="#">Basic Grid</a></li>
+                        <li><a href="#">Advanced Custom Fields</a></li>
+                        <li><a href="#">Widget & Mobile</a></li>
+                        <li><a href="#">Basic Grid</a></li>
+                        <li><a href="#">Advanced Custom Fields</a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <div class="demo_sc_lv_ul">
+                     <h3 class="demo_sc_h3">Grid View </h3>
+                     <ul>
+                        <li><a href="#">Basic Grid</a></li>
+                        <li><a href="#">Advanced Custom Fields</a></li>
+                        <li><a href="#">Widget & Mobile</a></li>
+                        <li><a href="#">Basic Grid</a></li>
+                        <li><a href="#">Advanced Custom Fields</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="demo_sc_lv_ul">
+               <h3 class="demo_sc_h3">FullCalendar Add-on</h3>
+               <ul>
+                  <li><a href="#">Basic Grid</a></li>
+                  <li><a href="#">Advanced Custom Fields</a></li>
+                  <li><a href="#">Widget & Mobile</a></li>
+               </ul>
+               <a href="" class="demo_sc_primary_btn">Buy Now</a>
+            </div>
+            <div class="demo_sc_lv_ul">
+               <h3 class="demo_sc_h3">FullCalendar add-on features:</h3>
+               <ul>
+                  <li>Week and day views added alongside the month view.</li>
+                  <li>Display event titles and start times directly on your calendar.</li>
+                  <li>Faster load times when navigating through months, weeks or days.</li>
+                  <li>Configure header navigation buttons to allow site visitors to easily switch between views.</li>
+                  <li>Intuitive design for multi-day events.</li>
+                  <li>Customizable calendar heights with optional scroll bars.</li>
+                  <li>Secured connection established between your site and Google using the OAuth standard.</li>
+                  <li>Works with our Google Calendar Pro add-on to display color-coded events within each view.</li>
+               </ul>
+               <a href="" class="demo_sc_primary_btn">See Pricing for the FullCalendar add-on.</a>
+            </div>
+            <div class="demo_sc_lv_ul">
+               <h3 class="demo_sc_h3">Grid View: Widget & Mobile</h3>
+               <p class="demo_sc_second_para">To the right is an example of a calendar grid view as it appears as a widget or on a mobile device.</p>
+               <p class="demo_sc_second_para"> With less room, dots are used to indicate the number of events on each page. A tap (on mobile devices) or a click or hover (on desktops) will bring up each days full details (all events).</p>
+               <p class="demo_sc_second_para"><span class="demo_sc_span_b">Scroll</span> down for an example of a widget/mobile view with the dark theme option set.</p>
+            </div>
+            <div class="demo_sc_lv_ul">
+               <h3 class="demo_sc_h3">Advanced Custom Fields</h3>
+               <a href="#" class="demo_sc_link">See screenshots and read more about the free Advanced Custom Fields add-on here.</a>
+               <a href="" class="demo_sc_primary_btn">See Pricing for the FullCalendar add-on.</a>
+            </div>
+            <div class="demo_sc_card_pink">
+               Come join us every Thursday for lunch, coffee, and some local blues music talent. Large groups please call ahead.
+            </div>
+            <div class="demo_sc_card_green">
+               Come join us every Thursday for lunch, coffee, and some local blues music talent. Large groups please call ahead.
+            </div>
+            <div class="demo_sc_card_bluish">
+               Come join us every Thursday for lunch, coffee, and some local blues music talent. Large groups please call ahead.
+            </div>
+            <div class="demo_sc_card_sand">
+               Come join us every Thursday for lunch, coffee, and some local blues music talent. Large groups please call ahead.
+            </div>
+         </div>
+         <div class="col-lg-5">
+            <div class="demo_sc_top_c">
+               <div class="demo_sc_inner">
+                  <img src="<?php echo $file_path; ?>/assets/images/demo_sc_img/top_sc_img.png" alt="calendar">
+               </div>
+            </div>
+            <div class="demo_sc_bottom_c">
+               <div class="demo_sc_inner">
+                  <img src="<?php echo $file_path; ?>/assets/images/demo_sc_img/bottom_sc_img.png" alt="calendar">
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="row">
+         <a href="#" class="demo_sc_grad_btn">Explore All Add-Ons</a>
+      </div>
+   </div>
+</div>
+<ul class="demo_sc_ul">
+                  <li><a href="#">Week and day view </a></li>
+                  <li>Display event titles and start times directly on your calendar.</li>
+                  <li>Faster load times when navigating through months, weeks or days.</li>
+                  <li>Configure header navigation buttons to allow site visitors to easily switch between views.</li>
+                  <li>Intuitive design for multi-day events.</li>
+                  <li>Customizable calendar heights with optional scroll bars.</li>
+                  <li>Secured connection established between your site and Google using the OAuth standard.</li>
+                  <li>Works with our Google Calendar Pro add-on to display color-coded events within each view.</li>
+               </ul>
+<footer class="demo_sc_main_footer">
+   <div class="container-fluid">
+      <div class="demo_sc_footer_container">
+         <div class="row demo_sc_footer_row ">
+            <div class="col-lg-6">
+               <div class="demo_sc_light_mode demo_sc_mode">
+                  <img src="<?php echo $file_path; ?>/assets/images/demo_sc_img/demo_sc_light_mode.png" alt="calendar">
+                  <span>Light Mode</span>
+               </div>
+            </div>
+            <div class="col-lg-6">
+               <div class="demo_sc_dark_mode demo_sc_mode">
+                  <img src="<?php echo $file_path; ?>/assets/images/demo_sc_img/demo_sc_dark_mode.png" alt="calendar">
+                  <span>Dark Mode</span>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="demo_sc_copyright_row demo_sc_pink_bg demo_sc_pink_text">
+         <div class="site_info">
+            <p>© Copyright <a href="https://xtendify.com/" target="_blank" class="demo_sc_pink_text   ">Xtendify Technologies</a> | <a href="https://simplecalendar.io/terms/">Terms of Service</a> | <a href="https://simplecalendar.io/privacy/">Privacy Policy</a></p>
+         </div>
+      </div>
+   </div>
+</footer>
+<?php
+get_footer();
